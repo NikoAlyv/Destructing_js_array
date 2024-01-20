937941cfc6a627152fed6c260e86084446df815c
NikoAlyv <enihad801@gmail.com>
2024-01-20 20:24:38 +0400

    task1

---
+++ app.js
@@ -0,0 +1,58 @@
+//TODO task1
+
+//!Part 1:
+
+// const numbers =[10,20,30,40,50]
+
+// const [firstNumber,secondNumber] = numbers
+
+
+// console.log(firstNumber,secondNumber);
+// console.log("Array langth:",numbers.length);
+
+//!Part 2:
+
+// const myString = "Azerbaycan"
+// const myNumber = 1918
+// const myBoolean = true
+
+// console.log(typeof myString);
+// console.log(typeof myNumber);
+// console.log(typeof myBoolean);
+
+//!Part 3:
+//?
+// const sum = firstNumber + secondNumber
+// const difference = secondNumber - firstNumber
+// const product = secondNumber *firstNumber
+// const quotient = secondNumber/firstNumber
+// const remainder = secondNumber%firstNumber
+// console.log("Cem:",sum);
+// console.log("Ferq:",difference)
+// console.log("Vuruq:",product)
+// console.log("Emsal",quotient)
+// console.log("Qaliq",remainder)
+
+//?
+
+// console.log(firstNumber==secondNumber)
+// console.log(firstNumber===secondNumber)
+// console.log(firstNumber!=secondNumber)
+// console.log(firstNumber!==secondNumber)
+// console.log(firstNumber >secondNumber)
+// console.log(firstNumber >secondNumber)
+// console.log(firstNumber >=secondNumber)
+// console.log(firstNumber <=secondNumber)
+
+
+//!Part 4:
+//?
+// console.log(firstNumber%2==0 && firstNumber>0)
+
+//?
+// console.log(firstNumber%2==0 || firstNumber>0);
+
+//?
+// console.log(!(firstNumber<0));
+
+
